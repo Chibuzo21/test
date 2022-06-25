5a547035384354363d046f3b3349c462d42b8683
Chibuzo21 <105220544+Chibuzo21@users.noreply.github.com>
2022-06-25 15:33:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let button = document.querySelector("button").addEventListener("click",Print())
-const Print =(name,height,country)=>{
 
-    let name = prompt("Type in Name","Name")
-let height = prompt("Type in height","height")
-let country = prompt("Type in country","country")
-    console.log("name:"+name ,"height: "+height,"country: "+country)
+const details = ["Ukaegbu Chibuzo","six foot 4","Nigeria"]
+
+const Print =()=>{
+
+    console.log(details)
 }
