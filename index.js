4411c7cf8a814c30fa0a09c4240f6d055456f14c
blessed <blessedemoefe1122334455@gmail.com>
2022-06-25 13:05:52 -0400

    done

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-let name = prompt("Type in Name","Name")
+
+const Print =(name,height,country)=>{
+
+    let name = prompt("Type in Name","Name")
 let height = prompt("Type in height","height")
 let country = prompt("Type in country","country")
-const Print =(name,height,country)=>{
     console.log("name:"+name ,"height: "+height,"country: "+country)
 }
