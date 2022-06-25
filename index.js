e6006cf4cd781c39f35d46008ac18c4f5b3af38d
Chibuzo21 <105220544+Chibuzo21@users.noreply.github.com>
2022-06-25 15:39:23 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let button = document.querySelector("button").addEventListener("click",Print())
 const details = ["Ukaegbu Chibuzo","six foot 4","Nigeria"]
 
 const Print =()=>{
-
+    alert(details),
     console.log(details)
 }
