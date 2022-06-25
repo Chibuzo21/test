84c4041c4a066b21e15bf184e2771fbec4dddbca
blessed <blessedemoefe1122334455@gmail.com>
2022-06-25 12:55:23 -0400

    done

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
- const detail=[
-    "Blossom",
-    "6feet  4",
-    "Nigeria"
-]
-
-
-const Print =()=>{
-    console.log(detail)
+let name = prompt("Type in Name","Name")
+let height = prompt("Type in height","height")
+let country = prompt("Type in country","country")
+const Print =(name,height,country)=>{
+    console.log("name:"+name ,"height: "+height,"country: "+country)
 }
